3a9a451788de1b33c7ce0325cf210e95b1eafb2c
Midori <midoric510@gmail.com>
2026-05-31 11:14:35 +0900

    変更　

--- js/app.js
+++ js/app.js
@@ -3,152 +3,89 @@ $(function () {
   let hands = ["狐", "鉄砲", "庄屋"];
 
   let enemyHand = "";
-
   let combo = 0;
-
+  let maxCombo = 0;
   let hp = 3;
 
   $("#startBtn").on("click", function () {
-
     startGame();
-
   });
 
   $("#restartBtn").on("click", function () {
-
     startGame();
-
   });
 
   $(".handBtn").on("click", function () {
+    let playerHand = $(this).data("hand");
 
-    let playerHand =
-      $(this).data("hand");
-
-    if (playerHand === "狐") {
-
-      playSound("foxSound");
-
-    }
-
-    else if (playerHand === "庄屋") {
-
-      playSound("shoyaSound");
-
-    }
-
-    else if (playerHand === "鉄砲") {
-
-      playSound("gunSound");
-
-    }
-
-    let randomNum =
-      Math.floor(Math.random() * hands.length);
-
-    enemyHand =
-      hands[randomNum];
+    let randomNum = Math.floor(Math.random() * hands.length);
+    enemyHand = hands[randomNum];
 
     $("#enemyHand").text(enemyHand);
 
     judge(playerHand, enemyHand);
 
     if (hp <= 0) {
-
       gameOver();
-
     }
-
   });
 
-  function playSound(id) {
-
-    const sound =
-      document.getElementById(id);
-
-    sound.currentTime = 0;
-
-    sound.play();
-
-  }
-
   function startGame() {
-
-    playSound("startSound");
-
     combo = 0;
-
+    maxCombo = 0;
     hp = 3;
 
     $("#combo").text(combo);
-
     $("#hp").text(hp);
-
     $("#enemyHand").text("？");
-
     $("#result").text("いざ、勝負！");
 
     $("#startBtn").hide();
-
     $(".game-over").hide();
-
     $(".game").show();
-
   }
 
   function judge(player, enemy) {
-
     if (player === enemy) {
-
       $("#result").text("あいこでござる！");
-
       combo = 0;
-
-    }
-
-    else if (
-
-      player === "狐" &&
-      enemy === "庄屋" ||
-
-      player === "庄屋" &&
-      enemy === "鉄砲" ||
-
-      player === "鉄砲" &&
-      enemy === "狐"
-
+    } else if (
+      player === "狐" && enemy === "庄屋" ||
+      player === "庄屋" && enemy === "鉄砲" ||
+      player === "鉄砲" && enemy === "狐"
     ) {
-
       $("#result").text("勝ちでござる！");
-
       combo += 1;
 
-    }
-
-    else {
-
+      if (combo > maxCombo) {
+        maxCombo = combo;
+      }
+    } else {
       $("#result").text("負けでござる…");
-
       hp -= 1;
-
       combo = 0;
-
     }
 
     $("#combo").text(combo);
-
     $("#hp").text(hp);
-
   }
 
   function gameOver() {
-
     $(".game").hide();
-
     $(".game-over").show();
 
-    $("#startBtn").show();
+    let rank = "";
 
+    if (maxCombo >= 5) {
+      rank = "東八拳の達人";
+    } else if (maxCombo >= 3) {
+      rank = "江戸の遊び人";
+    } else {
+      rank = "見習い拳士";
+    }
+
+    $("#rank").text(rank);
+    $("#startBtn").show();
   }
 
 });
